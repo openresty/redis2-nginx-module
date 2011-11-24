c866c1de0ca0f515b97b8d20ccd23be0c9d55b16
agentzh (章亦春) <agentzh@gmail.com>
2011-11-24 16:25:41 +0800

    bugfix: when "char" defaults to "unsigned char", the response parser based on ragel could not accept non-ascci bytes.

--- src/ngx_http_redis2_reply.c
+++ src/ngx_http_redis2_reply.c
@@ -34,15 +34,15 @@ ngx_http_redis2_process_reply(ngx_http_redis2_ctx_t *ctx,
     ngx_chain_t             **ll = NULL;
 
     int                       cs;
-    char                     *p;
-    char                     *orig_p;
+    signed char              *p;
+    signed char              *orig_p;
     ssize_t                   orig_len;
-    char                     *pe;
+    signed char              *pe;
 
     u = ctx->request->upstream;
     b = &u->buffer;
 
-    orig_p = (char *) b->last;
+    orig_p = (signed char *) b->last;
     orig_len = bytes;
 
     while (ctx->query_count) {
@@ -66,8 +66,8 @@ ngx_http_redis2_process_reply(ngx_http_redis2_ctx_t *ctx,
             dd("resumed the old state %d", cs);
         }
 
-        p  = (char *) b->last;
-        pe = (char *) b->last + bytes;
+        p  = (signed char *) b->last;
+        pe = (signed char *) b->last + bytes;
 
         dd("response body: %.*s", (int) bytes, p);
 
